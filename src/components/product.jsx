c3a5a777955de46e290450c831bfd7901325caca
ankita-tripathii <ankitatripathii011296@gmail.com>
2023-10-06 13:16:31 +0530

    add a new component product.jsx

---
+++ src/components/product.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+export default function Product() {
+
+  const [productList, setProductList] = useState([]);
+
+
+useEffect(() => {
+    fetchProductData();
+  }, []);
+
+
+  function fetchProductData() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => {
+        setProductList(json);
+      });
+  }
+
+
+  function product_detail(json) {
+
+    return json.map((product) => {
+
+      return (
+        <div id={product.id} key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+              <div className="card text-center h-100" key={product.id}>
+                <img
+                  className="card-img-top p-3"
+                  src={product.image}
+                  alt="Card"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {product.title.substring(0, 12)}...
+                  </h5>
+                  <p className="card-text">
+                    {product.description.substring(0, 90)}...
+                  </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item lead">$ {product.price}</li>
+                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Vestibulum at eros</li> */}
+                </ul>
+                <div className="card-body">
+                  <Link to={"/product/" + product.id} className="btn btn-dark m-1">
+                    Buy Now
+                  </Link>
+                  <button className="btn btn-dark m-1">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+      );
+    });
+  }
+
+
+  return(
+       
+       <div className="container my-3 py-3">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="display-5 text-center">Latest Products</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {product_detail(productList)}
+        </div>
+      </div>
+
+        
+    )
+
+}
